9b7abce76d58e8c9aa5dbce169e8b88a7d1b0910
AlvarocsED <acastellanos02@educarex.es>
2024-11-12 12:32:34 +0000

    1

--- cargaImagenes.sql
+++ cargaImagenes.sql
@@ -1,12 +1,20 @@
 insert into banderas values
 (null,curdate(),curdate(),'Alemania','Alemania.png'),
-
+(null,curdate(),curdate(),'Argentina','Argentinna.png'),
+(null,curdate(),curdate(),'Austria','Austria.png'),
+(null,curdate(),curdate(),'Belgica','Belgica.png'),
+(null,curdate(),curdate(),'Chile','Chile.png'),
+(null,curdate(),curdate(),'Colombia','Colombia.png'),
+(null,curdate(),curdate(),'Alemania','Costa_De_Marfil.png'),
+(null,curdate(),curdate(),'Dinamarca','Dinamarca.png'),
 (null,curdate(),curdate(),'Eslovenia','Eslovenia.png');
 
 insert into equipos values
 (null,curdate(),curdate(),'AFC Ajax','AFC_Ajax.png'),
-
-(null,curdate(),curdate(),'','');
+(null,curdate(),curdate(),'Atletico de Madrid','Atletico.png'),
+(null,curdate(),curdate(),'FC Barcelona','fc_Baecelona.png'),
+(null,curdate(),curdate(),'Real Madrid','Real-Madrid.png'),
+(null,curdate(),curdate(),'Sporting Portugal','Spoting_Lisboa.png');
 
 insert into jugadors values 
 (null,curdate(),curdate(),20000330, 'Delantero', 'Younes', 'Amin', 
