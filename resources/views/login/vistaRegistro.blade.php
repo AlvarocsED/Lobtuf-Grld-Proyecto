bd01b3ea6f675d26134156d28ce970fb83b1c1fb
AlvarocsED <acastellanos02@educarex.es>
2024-12-10 09:32:08 +0000

    rw

--- resources/views/login/vistaRegistro.blade.php
+++ resources/views/login/vistaRegistro.blade.php
@@ -1,115 +1,70 @@
 @extends('plantilla')
 @section('contenido')
 <!--Fomulario de registro-->
-<section class="vh-100" style="background-color: #eee;">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-lg-12 col-xl-11">
-          <div class="card text-black" style="border-radius: 25px;">
-            <div class="card-body p-md-5">
-              <div class="row justify-content-center">
-                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-  
-                  <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registro</p>
-  
-                  <form class="mx-1 mx-md-4" action="{{route('crearUsuario')}}" method="POST">  
-        @csrf
-        <div class="d-flex flex-row align-items-center mb-4">
-            <div data-mdb-input-init class="form-outline flex-fill mb-0">
-              <input type="text" id="name" name="name" class="form-control" />
-              <label class="form-label" for="name">Nombre</label>
-            </div>
-          </div>
-        @error('name')
-            <div class="text-danger">
-                Campo obligatorio
-            </div>
-        @enderror
-        <div class="d-flex flex-row align-items-center mb-4">
-            <div data-mdb-input-init class="form-outline flex-fill mb-0">
-              <input type="email" id="email" name="email" class="form-control" />
-              <label class="form-label" for="email">Email</label>
-            </div>
-          </div>
-        @error('email')
-            <div class="text-danger">
-                Campo obligatorio. Email ya en uso
-            </div>
-        @enderror
-        <div class="d-flex flex-row align-items-center mb-4">
-            <div data-mdb-input-init class="form-outline flex-fill mb-0">
-              <input type="password" id="password" name="password" class="form-control"/>
-              <label class="form-label" for="password">Contraseña</label>
-            </div>
-          </div>
-        @error('password')
-            <div class="text-danger">
-                Campo obligatorio
-            </div>
-        @enderror
-        <div class="d-flex flex-row align-items-center mb-4">
-            <div data-mdb-input-init class="form-outline flex-fill mb-0">
-              <input type="password" id="password2" name="password2" class="form-control"/>
-              <label class="form-label" for="password2">Repetir contraseña</label>
-            </div>
-          </div>
-        @error('password2')
-            <div class="text-danger">
-                Campo obligatorio. Contraseñas no coincidentes
-            </div>
-        @enderror
-        <div class="form-check d-flex justify-content-center mb-5">
-            <input class="form-check-input me-2" type="checkbox" value="" id="terms" name="terms"/>
-            <label class="form-check-label" for="terms">
-              Acepto los <a href="#">terminos y condiciones</a>
-            </label>
-          </div>
-          <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg" id="registrar">Registrarse</button>
-          </div>
-        <div>
-            <span class="display-7">@if (session('mensaje'))
-               {{ session('mensaje');}}
-            @endif</span>
-        </div>
-    </form>
-            </div>
-            <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                <img src="{{asset('img/registro/lmcr.jpg')}}"
-                  class="img-fluid" alt="Sample image">
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <script>
-    $(document).ready(function() {
-      // Función para habilitar/deshabilitar el botón
-      function toggleSubmitButton() {
-        const allFilled = $('#name').val().trim() !== "" &&
-                          $('#email').val().trim() !== "" &&
-                          $('#password').val().trim() !== "" &&
-                          $('#password2').val().trim() !== "";
-        const termsChecked = $('#terms').is(':checked');
-        
-        // Habilitar el botón si todos los campos están llenos y el checkbox está seleccionado
-        if (allFilled && termsChecked) {
-          $('#registrar').prop('disabled', false);
-        } else {
-          $('#registrar').prop('disabled', true);
-        }
-      }
-  
-      // Vincular eventos de cambio a los campos del formulario y el checkbox
-      $('#name, #email, #password, #password2, #terms').on('input change', function() {
-        toggleSubmitButton();
-      });
-  
-      // Comprobar el estado inicial al cargar la página
-      toggleSubmitButton();
-    });
-  </script>
-</section>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                  <p class="h1 text-center d-flex justify-content-center">Registro</p>
+                  <form action="{{route('crearUsuario')}}" method="POST" class="mx-1 mx-md-4">
+                    @csrf
+                      <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" data-darkreader-inline-fill="" style="--darkreader-inline-fill: currentColor;">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"></path>
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"></path>
+                          </svg>
+                        </span>
+                        <div class="form-floating">
+                          <input type="text" class="form-control" id=" name" name="name" placeholder="Nombre de Usuario">
+                          <label for="name">Nombre de Usuario</label>
+                        </div>
+                      </div>
+                      @error('name')
+                      <div class="text-danger">
+                          Campo obligatorio
+                      </div>
+                  @enderror          
+                      <div class="input-group mb-3">
+                        <span class="input-group-text">@</span>
+                        <div class="form-floating">
+                          <input type="email" class="form-control" id="email" name="email" placeholder="Introduce tu email">
+                          <label for="email">Email</label>
+                        </div>
+                      </div>
+                      @error('email')
+                        <div class="text-danger">
+                          Campo obligatorio. Email ya en uso
+                        </div>
+                      @enderror
+                      <div class="input-group mb-3">
+                        <span class="input-group-text">#</span>
+                      <div class="form-floating">
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña">
+                        <label for="password">Contraseña</label>
+                      </div>
+                    </div>
+                    @error('password')
+                      <div class="text-danger">
+                        Campo obligatorio
+                      </div>
+                    @enderror
+                    <div class="input-group mb-3">
+                      <span class="input-group-text">#</span>
+                    <div class="form-floating">
+                      <input type="password" class="form-control" id="password2" name="password2" placeholder="Repetie Contraseña">
+                      <label for="password2">Repetir Contraseña</label>
+                    </div>
+                  </div>
+                  @error('password2')
+                    <div class="text-danger">
+                      Campo obligatorio
+                    </div>
+                  @enderror
+                  </div>
+                  </form>
+                </div>
+                <div class="d-flex justify-content-center">
+                  <img src="{{asset('img/registro/lmcr.jpg')}}" alt="LMCR">
+                </div>
+                </div>
 @endsection
