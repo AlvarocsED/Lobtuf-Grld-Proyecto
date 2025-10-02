7e9a5f79c064ec7406b0811ed2419ab819312803
AlvarocsED <acastellanos02@educarex.es>
2025-10-02 08:12:48 +0000

    sql

---
+++ imagenes.sql
@@ -0,0 +1,6 @@
+insert into banderas values
+(1, 'Alemania', 'Alemania.png'),
+(2, 'Arggntina', 'Arggntina.png'),
+(3, 'Austria', 'Austria.png'),
+(4, 'Belgica', 'Belgica.png').
+                                                                     
