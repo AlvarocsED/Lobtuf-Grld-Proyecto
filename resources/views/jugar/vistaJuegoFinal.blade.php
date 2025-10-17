a7df54c9a0c99cee149ba1cee7029e8724a3edfa
AlvarocsED <acastellanos02@educarex.es>
2025-10-17 11:24:00 +0000

    tabjla

--- resources/views/jugar/vistaJuegoFinal.blade.php
+++ resources/views/jugar/vistaJuegoFinal.blade.php
@@ -3,45 +3,34 @@
     Juego
 @endsection
 @section('contenido')
-<div class="border-bottom border-imfo border-bottom-2">
-  <h1 class="fw-bold">LOBTUF GRID</h1>
-</div>
-  <table class="grid-table mt-4">
-    <tr>
-      <th></th>
-      <th><img src=//bandera1 class="flag-icon" alt="//nombreBamdera1"><br><small>FRANCE</small></th>
-      <th><img src="https://flagcdn.com/ch.svg" class="flag-icon" alt="Switzerland"><br><small>SWITZERLAND</small></th>
-      <th><img src="https://flagcdn.com/ng.svg" class="flag-icon" alt="Nigeria"><br><small>NIGERIA</small></th>
-    </tr>
-    <tr>
-      <th><img src="https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg" alt="Liverpool" style="height:40px;"><br><small>LIVERPOOL</small></th>
-      <td><img src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Sadio_Mané_2018.jpg" alt="Sadio Mané"></td>
-      <td><img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Xherdan_Shaqiri_2018.jpg" alt="Shaqiri"></td>
-      <td></td>
-    </tr>
-    <tr>
-      <th><img src="https://upload.wikimedia.org/wikipedia/en/0/0b/Inter_Milan.svg" alt="Inter" style="height:40px;"><br><small>INTER</small></th>
-      <td><img src="https://upload.wikimedia.org/wikipedia/commons/5/56/Khouma_Babacar.jpg" alt="Babacar"></td>
-      <td><img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Valon_Behrami_2018.jpg" alt="Behrami"></td>
-      <td></td>
-    </tr>
-    <tr>
-      <th><img src="https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg" alt="Chelsea" style="height:40px;"><br><small>CHELSEA</small></th>
-      <td><img src="https://upload.wikimedia.org/wikipedia/commons/4/45/Nicolas_Anelka.jpg" alt="Anelka"></td>
-      <td><img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Dennis_Zakaria_2021.jpg" alt="Zakaria"></td>
-      <td></td>
-    </tr>
+<div class="container text-center">
+  <h1 class="futbol11-title">FUTBOL11 GRID</h1>
+
+  <table class="table grid-container table-dark table-bordered align-center">
+    <thead class="grid-header">
+      <tr>
+        <th>FUTBOL11 GRID</th>
+        <th><img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Juventus_FC_2017_logo.svg" class="team-logo" alt="Juventus"><br>JUVENTUS</th>
+        <th><img src="https://upload.wikimedia.org/wikipedia/commons/4/49/Flag_of_Ukraine.svg" class="flag" alt="Ucrania"><br>UKRAINE</th>
+        <th><img src="https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg" class="flag" alt="Alemania"><br>GERMANY</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th><img src="https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg" class="team-logo" alt="Man City"><br>MAN CITY</th>
+        <td></td><td></td><td></td>
+      </tr>
+      <tr>
+        <th><img src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Logo_of_AC_Milan.svg" class="team-logo" alt="AC Milan"><br>AC MILAN</th>
+        <td></td><td></td><td></td>
+      </tr>
+      <tr>
+        <th><img src="https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg" class="team-logo" alt="Chelsea"><br>CHELSEA</th>
+        <td></td><td></td><td></td>
+      </tr>
+    </tbody>
   </table>
 
-  <div class="form-container">
-    <form class="d-flex justify-content-center">
-      <input type="text" class="form-control w-25 me-2" placeholder="Type the footballer here">
-      <button type="submit" class="btn btn-light">↩️</button>
-    </form>
-    <p class="mt-3 text-muted">Anelka has been added on the only possible spot</p>
-  </div>
-    </main>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+  <input type="text" class="form-control w-50 mx-auto mt-3" placeholder="Type the footballer here">
+</div>
 @endsection
